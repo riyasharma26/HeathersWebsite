f55261e3304cbe9042b58521c11c3890857427d6
Riya <36569973+riyasharma26@users.noreply.github.com>
2026-05-14 22:50:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Paws & Portraits", layout="wide")
 
-with open("paws_and_portraits.html", "r") as f:
+with open("index.html", "r") as f:
     html_content = f.read()
 
 st.components.v1.html(html_content, height=4000, scrolling=True)
